2779e46c46f919dbe65721cf90a21a046942108a
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-09-27 23:24:16 +0530

    Create sort1_p2.java

---
+++ sort1_p2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Collections;
+public class sort1{
+        public static void main(String[]args){
+                List<int[]>frame=new ArrayList<>();
+                System.out.println("Enter no. of frames:");
+                Scanner sc= new Scanner(System.in);
+                int n =sc.nextInt();
+                int seqNum=1;
+                for(int i=0;i<n;i++){
+                        Random random=new Random();
+                        seqNum=seqNum+random.nextInt(1000);
+                        System.out.printf("Enter data  for %d frame>> ",i+1);
+                        int data=sc.nextInt();
+                        frame.add(new int []{seqNum,data});
+                }
+                System.out.println("\n\nBefore sorting>> ");
+                for(int[] i:frame){
+                        System.out.printf("seqNum-->%d, Data-->%d\n",i[0],i[1]);
+                }
+                frame=sortFrame(frame);
+                System.out.println("\n\nAfter sorting>> ");
+                for(int [] i :frame){
+                        System.out.printf("seqNum-->%d,Data-->%d\n",i[0],i[1]);
+                }
+        }
+        public static List<int[]>sortFrame(List<int[]>frame){
+                Collections.sort(frame,(a , b) -> Integer.compare(a[0] , b[0]));
+                return frame;
+        }
+}
