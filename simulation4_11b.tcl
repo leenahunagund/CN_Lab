bc740d6c83ff933062ce0d0021e4efd766c0b7c1
Leena <109791804+leenahunagund@users.noreply.github.com>
2024-01-09 21:39:04 +0530

    Update simulation4_11b.tcl

--- simulation4_11b.tcl
+++ simulation4_11b.tcl
@@ -3,32 +3,36 @@ set ns [new Simulator]
 
 #Setup topography object
 set topo [new Topography]
-$topo load_flatgrid 1500 1500
+$topoload_flatgrid 1500 1500
 
 #Open the NS trace file
-set tracefile [open pr6.tr w]
+set tracefile [open p6.tr w]
 $ns trace-all $tracefile
 
 #Open the NAM trace file
-set namfile [open pr6.nam w]
+set namfile [open p6.nam w]
 $ns namtrace-all $namfile
 $ns namtrace-all-wireless $namfile 1500 1500
 
-
+#===================================
+# Mobile node parameter setup
+#===================================
 $ns node-config -adhocRouting DSDV \
-$ns node-config -llType LL \
-$ns node-config -macType Mac/802_11 \
-$ns node-config -ifqType Queue/DropTail \
-$ns node-config -ifqLen 20 \
-$ns node-config -phyType Phy/WirelessPhy \
-$ns node-config -channelType Channel/WirelessChannel \
-$ns node-config -propType Propagation/TwoRayGround \
-$ns node-config -antType Antenna/OmniAntenna \
-$ns node-config -topoInstance $topo \
-$ns node-config -agentTrace ON \
-$ns node-config -routerTrace ON
+	-llType LL \
+	-macType Mac/802_11 \
+	-ifqType Queue/DropTail \
+	-ifqLen 20 \
+	-phyTypePhy/WirelessPhy \
+	-channelType Channel/WirelessChannel \
+	-propType Propagation/TwoRayGround \
+	-antType Antenna/OmniAntenna \
+	-topoInstance $topo \
+	-agentTrace ON \
+	-routerTrace ON
 
-
+#===================================
+# Nodes Definition
+#===================================
 create-god 6
 #Create 6 nodes
 set n0 [$ns node]
@@ -62,7 +66,9 @@ $n5 set Y_ 177
 $n5 set Z_ 0.0
 $ns initial_node_pos $n5 20
 
-
+#===================================
+# Agents Definition
+#===================================
 #Setup a UDP connection
 set udp0 [new Agent/UDP]
 $ns attach-agent $n0 $udp0
@@ -78,43 +84,47 @@ set sink1 [new Agent/TCPSink]
 $ns attach-agent $n5 $sink1
 $ns connect $tcp0 $sink1
 
-
+#===================================
+# Applications Definition
+#===================================
 #Setup a CBR Application over UDP connection
 set cbr0 [new Application/Traffic/CBR]
 $cbr0 attach-agent $udp0
 $cbr0 set packetSize_ 1000
-$cbr0 set rate_ 1.743Mb
+$cbr0 set rate_ 1.0Mb
 $cbr0 set random_ null
 
 #Setup a FTP Application over TCP connection
 set ftp0 [new Application/FTP]
 $ftp0 attach-agent $tcp0
 
-
+#===================================
+# Termination
+#===================================
 #Define a 'finish' procedure
 proc finish {} {
-global ns tracefile namfile
+global ns tracefilenamfile
 $ns flush-trace
 close $tracefile
 close $namfile
-exec nam pr6.nam &
+exec nam p6.nam &
 exec echo "Number of packets dropped is : " &
-exec grep -c "^D" pr6.tr &
-exec awk -f pr6.awk pr6.tr &
+exec grep -c "^D" p6.tr &
 exit 0
 }
 
 $ns at 1.0 "$cbr0 start"
 $ns at 2.0 "$ftp0 start"
-$ns at 18.0 "$ftp0 stop"
-$ns at 20.0 "$cbr0 stop"
-$ns at 20.0 "finish"
-$ns at 7 "$n4 setdest 100 60 20"
-$ns at 10 "$n4 setdest 700 300 20"
-$ns at 15 "$n4 setdest 900 200 20"
+$ns at 180.0 "$ftp0 stop"
+$ns at 200.0 "$cbr0 stop"
+$ns at 200.0 "finish"
+$ns at 70 "$n4 setdest 100 60 20"
+$ns at 100 "$n4 setdest 700 300 20"
+$ns at 150 "$n4 setdest 900 200 20"
 $ns run
 
-******* awk file******
+
+// AWK file (filename.awk)
 BEGIN{
 count1=0
 count2=0
@@ -123,6 +133,7 @@ pack2=0
 time1=0
 time2=0
 }
+
 {
 if($1=="r" && $3=="_1_" && $4=="RTR")
 {
@@ -130,6 +141,7 @@ count1++
 pack1=pack1+$8
 time1=$2
 }
+
 if($1=="r" && $3=="_2_" && $4=="RTR")
 {
 count2++
@@ -137,6 +149,7 @@ pack2=pack2+$8
 time2=$2
 }
 }
+
 END{
 printf("The Throughput from n0 to n1: %f Mbps \n", ((count1*pack1*8)/(time1*1000000)));
 printf("The Throughput from n1 to n2: %f Mbps \n", ((count2*pack2*8)/(time2*1000000)));
